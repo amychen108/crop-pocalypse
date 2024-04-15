5ea52fd115d00e2a370fba0b4cb26452d906cb7d
Hachi 8 <justin.wang@slu.edu>
2024-04-15 15:29:04 -0500

    hitbox change on house

--- src/view/FarmHand.gd
+++ src/view/FarmHand.gd
@@ -3,12 +3,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("hi") # Replace with function body.
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
