fa8d71b4e37158eace4a662ad348c47fc9d95452
Hachi 8 <justin.wang@slu.edu>
2024-04-15 19:16:57 -0500

    level 1 tutorial pop up done (can adjust font/size/etc later)

--- src/view/TutorialHUD.gd
+++ src/view/TutorialHUD.gd
@@ -1,17 +1,16 @@
 extends CanvasLayer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_tree().paused = true
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
 
 func _on_button_pressed():
 	print("tutorial label closed")
 	$Button.visible = false;
 	$Label.visible = false;
+	get_tree().paused = false
