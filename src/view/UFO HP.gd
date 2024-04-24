eff2c387a5aa3dd5ec5cc72c11e3d4c0fe4f10ce
Hachi 8 <justin.wang@slu.edu>
2024-04-24 00:07:08 -0500

    level 3 finished

---
+++ src/view/UFO HP.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var HP = 4
+var scene_tree
+
+func _ready():
+	scene_tree = get_tree()
+	$ufoHP.text = "UFO Health: " + str(HP)
+	
+func lose_health():
+	HP-= 1
+	$ufoHP.text = "UFO Health: " + str(HP)
+	
+func get_health():
+	return HP
--- src/view/UFO.gd
+++ src/view/UFO.gd
@@ -2,18 +2,36 @@ extends Area2D
 
 var scene_tree
 var scoreLabel
+var healthLabel
 var lastSpawn = 0
 var scoreInterval = 5
+var flag = 0
 
 func _ready():
 	scene_tree = get_tree()
 	scoreLabel = get_node("/root/Level3/Tractor3/Score")
+	healthLabel = get_node("/root/Level3/UFO/ufoHealth")
 
 func _process(delta):
 	if scoreLabel.get_score() >= lastSpawn + scoreInterval:
 		lastSpawn += scoreInterval
-		print("spawn ufo")
-
+		flag = 1
+		position.x = randf_range(-500, 1200)
+		position.y = -200
+		
 func _on_area_entered(node):
 	if node.name == "Tractor3":
-		get_tree().change_scene_to_file("res://YouWin.tscn") 
+		healthLabel.lose_health()
+		$CLANK.play()
+		flag = 0
+		if healthLabel.get_health() == 0:
+			get_tree().change_scene_to_file("res://YouWin.tscn") 
+
+func _physics_process(delta):
+	if flag == 0:
+		if position.y > -200:
+			position.y+= -40
+		
+	if flag == 1:
+		position.x += 4
+		position.y += 18
