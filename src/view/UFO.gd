9a9a5c5dc2df574473aec0af1b030ae97afca6fb
Hachi 8 <justin.wang@slu.edu>
2024-04-23 19:56:17 -0500

    ufo spawn interval logic made, need to work on spawning the ufo at said intervals

--- src/view/UFO.gd
+++ src/view/UFO.gd
@@ -1,12 +1,18 @@
 extends Area2D
 
 var scene_tree
+var scoreLabel
+var lastSpawn = 0
+var scoreInterval = 5
 
 func _ready():
 	scene_tree = get_tree()
+	scoreLabel = get_node("/root/Level3/Tractor3/Score")
 
 func _process(delta):
-	pass
+	if scoreLabel.get_score() >= lastSpawn + scoreInterval:
+		lastSpawn += scoreInterval
+		print("spawn ufo")
 
 func _on_area_entered(node):
 	if node.name == "Tractor3":
