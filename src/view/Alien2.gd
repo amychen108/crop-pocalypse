a94105abf8d0545e08ee5f8c5bef3aa2c39e76bb
Hachi 8 <justin.wang@slu.edu>
2024-04-29 03:12:42 -0500

    added some basic functionality for lv 2

--- src/view/Alien2.gd
+++ src/view/Alien2.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 var player
+var alienHP
 
 func _ready():
 	player = get_node("/root/Level2/Farmer")
 	$AlienN.visible = false
+	alienHP = 3
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
--- src/view/Farmer.gd
+++ src/view/Farmer.gd
@@ -6,6 +6,9 @@ func _physics_process(delta):
 	var screensize = get_viewport_rect().size
 	var input = Vector2()
 	
+	$Hurtbox.disabled = false
+	$Hitbox.disabled = true
+	
 	if Input.is_key_pressed(KEY_W):
 		input.y = -n
 	elif Input.is_key_pressed(KEY_S):
@@ -18,7 +21,13 @@ func _physics_process(delta):
 	position += input
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
+	
+	if Input.is_key_pressed(KEY_SPACE):
+		print("hi")
+		$Hurtbox.disabled = true
+		$Hitbox.disabled = false
 
 func _on_body_entered(body):
-	$Health.lose_life()
-	$OOF.play()
+	if($Hurtbox.disabled == false):
+		$Health.lose_life()
+		$OOF.play()
