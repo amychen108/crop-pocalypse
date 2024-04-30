d6ee87a3b8eb11cdb69be54d18dfd2875df3092d
dudered00 <88799553+dudered00@users.noreply.github.com>
2024-04-30 18:51:57 -0500

    Update unit-test.cpp

--- unit-test.cpp
+++ unit-test.cpp
@@ -2,8 +2,8 @@
 
 #include <iostream>
 #include <array>
-#include "/src/Controller/health-calc.cpp"
-#include "/src/Controller/high-score.cpp"
+#include "../src/Controller/health-calc.cpp"
+#include "../src/Controller/high-score.cpp"
 
 void testHealthCalc() {
     std::cout << "Testing Health Calculation, Damage Taken";
