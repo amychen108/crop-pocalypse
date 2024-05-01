2e71272462544cb26437b179f05e6b69a4f36bdf
dudered00 <88799553+dudered00@users.noreply.github.com>
2024-04-30 19:15:50 -0500

    Update unit-test.cpp

--- unit-test.cpp
+++ unit-test.cpp
@@ -5,21 +5,22 @@
 #include "src/Controller/health-calc.cpp"
 #include "src/Controller/high-score.cpp"
 
-void testHealthCalc() {
+bool testHealthCalc() {
     std::cout << "Testing Health Calculation, Damage Taken\n";
     HealthCalc myhealth = HealthCalc();
     int result = 2;
     myhealth.take_damage();
     if (result == myhealth.get_health()) {
         std::cout << "Test Passed: Damage taken once, health is 2\n";
+        return true;
     }
     else {
         std::cerr << "Test Failed\n";
+        return false;
     }
-
 }
 
-void testHighScore() {
+bool testHighScore() {
     std::cout << "Testing High Scores, numbers are 17, 25, 21\n";
     HighScore scores = HighScore();
     scores.insert_new_score(17); // should be moved to first
@@ -28,28 +29,34 @@ void testHighScore() {
 
     int * thescores = scores.get_scores();
 
-    if (thescores[0] == 25) {
-        if (thescores[1] == 21) {
-            if (thescores[2] == 17) {
-                std::cout << "Test Passed: High Scores are 25, 21, 17\n";
-            }
-            else {
-                std::cerr << "Test Failed\n";
-            }
-        }
-        else {
-            std::cerr << "Test Failed\n";
-        }
+    if (thescores[0] == 25 && thescores[1] == 21 && thescores[2] == 17) {
+        std::cout << "Test Passed: High Scores are 25, 21, 17\n";
+        return true;
     }
     else {
         std::cerr << "Test Failed\n";
+        return false;
     }
-
 }
 
 int main() {
-    testHealthCalc();
-    testHealthCalc();
+    int passedTests = 0;
+    int failedTests = 0;
+
+    if (testHealthCalc()) {
+        passedTests++;
+    } else {
+        failedTests++;
+    }
+
+    if (testHighScore()) {
+        passedTests++;
+    } else {
+        failedTests++;
+    }
+
+    std::cout << "Tests Passed: " << passedTests << std::endl;
+    std::cout << "Tests Failed: " << failedTests << std::endl;
 
     return 0;
 }
