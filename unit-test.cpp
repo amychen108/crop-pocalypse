fb8bfb33710f1bc32dcf42d964b9f39243431500
dudered00 <88799553+dudered00@users.noreply.github.com>
2024-04-30 19:21:50 -0500

    Update unit-test.cpp

--- unit-test.cpp
+++ unit-test.cpp
@@ -20,6 +20,42 @@ bool testHealthCalc() {
     }
 }
 
+bool testHealthCalc2() {
+    std::cout << "Testing Health Calculation, Damage Taken Twice\n";
+    HealthCalc myhealth = HealthCalc();
+    int result = 1;
+    myhealth.take_damage();
+    myhealth.take_damage();
+    if (result == myhealth.get_health()) {
+        std::cout << "Test Passed: Damage taken once, health is 1\n";
+        return true;
+    }
+    else {
+        std::cerr << "Test Failed\n";
+        return false;
+    }
+}
+
+bool testHealthCalc3() {
+    std::cout << "Testing Health Calculation, Damage Taken More than 3 times\n";
+    HealthCalc myhealth = HealthCalc();
+    int result = 0;
+    myhealth.take_damage();
+    myhealth.take_damage();
+    myhealth.take_damage();
+    myhealth.take_damage();    
+    myhealth.take_damage();
+    myhealth.take_damage();
+    if (result == myhealth.get_health()) {
+        std::cout << "Test Passed: Damage taken once, health is 0\n";
+        return true;
+    }
+    else {
+        std::cerr << "Test Failed\n";
+        return false;
+    }
+}
+
 bool testHighScore() {
     std::cout << "Testing High Scores, numbers are 17, 25, 21\n";
     HighScore scores = HighScore();
@@ -39,6 +75,26 @@ bool testHighScore() {
     }
 }
 
+bool testHighScore2() {
+    std::cout << "Testing High Scores, numbers are 17, 25, 21, 84\n";
+    HighScore scores = HighScore();
+    scores.insert_new_score(17); // should be moved to first
+    scores.insert_new_score(25); // should be moved to first, other should be second
+    scores.insert_new_score(21); // should be in second
+    scores.insert_new_score(84); //should replace first, knock down the rest
+
+    int * thescores = scores.get_scores();
+
+    if (thescores[0] == 84 && thescores[1] == 25 && thescores[2] == 21) {
+        std::cout << "Test Passed: High Scores are 84, 25, 21\n";
+        return true;
+    }
+    else {
+        std::cerr << "Test Failed\n";
+        return false;
+    }
+}
+
 int main() {
     int passedTests = 0;
     int failedTests = 0;
@@ -48,8 +104,25 @@ int main() {
     } else {
         failedTests++;
     }
+        if (testHealthCalc2()) {
+        passedTests++;
+    } else {
+        failedTests++;
+    }
+    
+     if (testHealthCal3()) {
+        passedTests++;
+    } else {
+        failedTests++;
+    }
 
     if (testHighScore()) {
+        passedTests++;
+    } else {
+        failedTests++;
+    }
+
+    if (testHighScore2()) {
         passedTests++;
     } else {
         failedTests++;
